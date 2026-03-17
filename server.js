de01e74dbd04d09cf7e926bae1dc7cd85cdb1f49
yz7e9 <240876835+yz7e9@users.noreply.github.com>
2026-03-17 17:24:23 +0100

    Data & Server

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import camisetasRouter from './routes/camisetas.route.js';
+import comandasRouter from './routes/comandas.route.js';
+
+const app = express();
+const PORT = 3001;
+
+// Middlewares globales
+app.use(express.json());
+
+// Log mínimo
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
+
+// Rutas
+app.use('/api/camisetas', camisetasRouter);
+app.use('/api/comandas', comandasRouter);
+
+// Middleware de errores
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).json({ message: "Error interno" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}/`);
+});
